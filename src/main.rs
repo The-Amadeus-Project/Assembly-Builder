3b474a33967a2f8b8096be1a96981a867d69f699
CunningBard <jhonjhonjumaday@gmail.com>
2022-08-01 12:14:57 +0800

    tests

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,29 @@ mod b_lib;
 
 fn main() {
     let mut asm_builder = builder::Builder::new_program("main");
-    asm_builder.new_string_literal("name", "eric\n");
+    asm_builder.new_string_literal("put_i_fmt_str", "%d\n");
+    asm_builder.new_string_literal("put_s_fmt_str", "%s\n");
+    asm_builder.new_string_literal("string_1", "mongrel\n");
     asm_builder.extern_add( "printf");
 
+    asm_builder.open_function("put_i");
+    asm_builder.mov("esi", "edi");
+    asm_builder.mov("eax", "0");
+    asm_builder.call_function("printf", vec!["put_i_fmt_str"]);
+    asm_builder.close_function();
+
+    asm_builder.open_function("put_s");
+    asm_builder.mov("esi", "edi");
+    asm_builder.mov("eax", "0");
+    asm_builder.call_function("printf", vec!["put_s_fmt_str"]);
+    asm_builder.close_function();
+
     asm_builder.open_function("main");
-    asm_builder.mov("rdi", "name");
-    asm_builder.mov("al", "0");
-    asm_builder.call("printf");
+    asm_builder.new_local_dword(32);
+    asm_builder.add("dword [rsp - 4]", "10");
+    asm_builder.call_function("put_i", vec!["dword [rsp - 4]"]);
 
-    asm_builder.new_local_dword(100);
-
+    asm_builder.call_function("put_s", vec!["string_1"]);
 
     asm_builder.mov("rax","0");
 
